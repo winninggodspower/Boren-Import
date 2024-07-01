3d2393763f2c3654a8c24e6aee39c5a60fc06008
winning godspower <winninggodspower@gmail.com>
2024-07-01 08:59:29 +0100

    fix url bug

--- service_enquiry/urls.py
+++ service_enquiry/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("procurement_form", views.procurement_form, name="procurement_form"),
-    path("customer_fx_form", views.customer_fx_form, name="customer_fx_form")
-    path("risk_attestation_form", views.views.risk_attestation_form, name="risk_attestation_form")
+    path("customer_fx_form", views.customer_fx_form, name="customer_fx_form"),
+    path("risk_attestation_form", views.risk_attestation_form, name="risk_attestation_form")
 ]
